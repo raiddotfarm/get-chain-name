691aa425473bb30d82b9c5c12372fbfd2cc9d99c
ayush <62694274+ayshptk@users.noreply.github.com>
2022-04-11 21:12:39 +0530

    lmao

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // DATA TAKEN FROM https://chainid.network/chains.json
-const fs = require("fs");
+const chains = require("./chains.json");
 module.exports = function get(chainId = 1, getData = false) {
-  const chains = fs.readFileSync(`${__dirname}/chains.json`, "utf8");
-  const chain = JSON.parse(chains).find((c) => c.chainId === chainId);
+  const chain = chains.find((c) => c.chainId === chainId);
   if (getData) {
     return chain;
   }
